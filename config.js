0609285426f190558c2da4039a544c1a31cfa319
malincarlsson11-shine <malin.carlsson11@gmail.com>
2026-04-08 16:29:58 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://biqtstbtmchnystomvvt.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcXRzdGJ0bWNobnlzdG9tdnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1Nzc3OTksImV4cCI6MjA5MTE1Mzc5OX0.Mqf22SPfj2aio\_uFwvVLA9dzB-6LEu7ekI\_y-JvxukY',
+  GOOGLE_MAPS_API_KEY: 'AIzaSyDbx8AWLfg8BdkjgJ-3uRFJr-C4WUmNlpI'
+};
